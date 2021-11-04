ac1861d7555dbce88697eeb4fd5c5bb0049aae31
luizlcezario <luizlcezario@gmail.com>
2021-11-04 18:28:54 -0300

    final

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: luizz <luizz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/24 13:28:39 by llima-ce          #+#    #+#             */
-/*   Updated: 2021/11/04 18:02:49 by luizz            ###   ########.fr       */
+/*   Updated: 2021/11/04 18:26:02 by luizz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,7 @@ int main(void)
 	int number = 30;
 	
 	printf("TESTE STRING\033[0m\n");
-	ft_printf("%.0s", "hi there");
-	printf("%.0s", "hi there");
+	ft_printf("%.0d", 420000);
 	// ft_printf(" %.2s %.3s %.4s %.5s %.1s ", " - ", "", "4", "", "2 ");
 	// printf(" len = %d\n",printf(" %.2s %.3s %.4s %.5s %.1s ", " - ", "", "4", "", "2 "));
 	// printf(" len = %d\n",ft_printf(" %02d ",-1));
