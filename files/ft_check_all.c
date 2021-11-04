e23c36bcab6cfe0e98f72cc29e09c57d40f718ba
luizlcezario <luizlcezario@gmail.com>
2021-11-04 18:30:44 -0300

    final

--- files/ft_check_all.c
+++ files/ft_check_all.c
@@ -6,7 +6,7 @@
 /*   By: luizz <luizz@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 18:19:52 by llima-ce          #+#    #+#             */
-/*   Updated: 2021/11/04 18:29:58 by luizz            ###   ########.fr       */
+/*   Updated: 2021/11/04 18:30:40 by luizz            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,8 @@ int	check_pointers(char *flag, t_format *buffer)
 		buffer->cf_now->flag_pointer += flag[a] - 48;
 		a++;
 	}
-	if ((a == 1 && buffer->cf_now->flag_pointer == 0)
+	if ((a >= 1 && buffer->cf_now->flag_pointer == 0
+	&& buffer->cf_now->conversion != 'd' )
 	|| (buffer->cf_now->conversion == 's' && a == 2
 	&& buffer->cf_now->flag_pointer == 0))
 		buffer->cf_now->flag_pointer = -1;
